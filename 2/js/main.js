e59682f18b41e990af66fadbe307aa5862338ec2
Keks <keks@htmlacademy.ru>
2020-02-05 22:25:57 +0300

    :heavy_check_mark: Сборка #2

--- 2/js/main.js
+++ 2/js/main.js
@@ -1,10 +1,5 @@
 'use strict';
 
-var map = document.querySelector('.map');
-var MAP_PIN = document.querySelector('#pin')
-  .content
-  .querySelector('.map__pin');
-var mapPinsList = document.querySelector('.map__pins');
 var TIMES = ['12:00', '13:00', '14:00'];
 var TYPES = ['palace', 'flat', 'house', 'bungalo'];
 var FEATURES = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
@@ -13,7 +8,8 @@ var PHOTOS = [
   'http://o0.github.io/assets/images/tokyo/hotel2.jpg',
   'http://o0.github.io/assets/images/tokyo/hotel3.jpg'
 ];
-var POSSIBLE_LOCATIONS = {
+var COUNT_OFFERS = 8;
+var PossibleLocations = {
   x: {
     min: 0,
     max: 1200
@@ -24,21 +20,30 @@ var POSSIBLE_LOCATIONS = {
   }
 };
 
-var randomInt = function (min, max) {
+var mapElement = document.querySelector('.map');
+var mapPinElement = document.querySelector('#pin')
+  .content
+  .querySelector('.map__pin');
+var mapPinsListElement = document.querySelector('.map__pins');
+
+
+var getRandomIntFromRange = function (min, max) {
   return Math.round(min - 0.5 + Math.random() * (max - min + 1));
 };
-var arrayRandElement = function (array) {
+
+var getRandomArrayElement = function (array) {
   return array[Math.floor(Math.random() * array.length)];
 };
+
 var getArrayRandomLength = function (array) {
-  return array.slice(0, randomInt(1, array.length + 1));
+  return array.slice(0, getRandomIntFromRange(1, array.length + 1));
 };
 
-var generateOffersData = function (countOffets) {
+var generateOffersData = function () {
   var offersData = [];
-  for (var i = 1; i <= countOffets; i++) {
-    var locationX = randomInt(POSSIBLE_LOCATIONS.x.min, POSSIBLE_LOCATIONS.x.max);
-    var locationY = randomInt(POSSIBLE_LOCATIONS.y.min, POSSIBLE_LOCATIONS.y.max);
+  for (var i = 1; i <= COUNT_OFFERS; i++) {
+    var locationX = getRandomIntFromRange(PossibleLocations.x.min, PossibleLocations.x.max);
+    var locationY = getRandomIntFromRange(PossibleLocations.y.min, PossibleLocations.y.max);
     offersData[i - 1] = {
       author: {
         avatar: 'img/avatars/user0' + i + '.png'
@@ -46,12 +51,12 @@ var generateOffersData = function (countOffets) {
       offer: {
         title: 'Lorem ipsum dolor sit amet',
         address: locationX + ', ' + locationY,
-        price: randomInt(10000, 99000),
-        type: arrayRandElement(TYPES),
-        rooms: randomInt(1, 6),
-        guests: randomInt(2, 5),
-        checkin: arrayRandElement(TIMES),
-        checkout: arrayRandElement(TIMES),
+        price: getRandomIntFromRange(10000, 99000),
+        type: getRandomArrayElement(TYPES),
+        rooms: getRandomIntFromRange(1, 6),
+        guests: getRandomIntFromRange(2, 5),
+        checkin: getRandomArrayElement(TIMES),
+        checkout: getRandomArrayElement(TIMES),
         features: getArrayRandomLength(FEATURES),
         description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto culpa dicta, iure nesciunt omnis recusandae.',
         photos: getArrayRandomLength(PHOTOS)
@@ -64,10 +69,11 @@ var generateOffersData = function (countOffets) {
   }
   return offersData;
 };
+
 var renderOffers = function (offersArray) {
   var fragment = document.createDocumentFragment();
   offersArray.forEach(function (item) {
-    var offerElement = MAP_PIN.cloneNode(true);
+    var offerElement = mapPinElement.cloneNode(true);
     var offerImg = offerElement.querySelector('img');
     var coorX = item.location.x + offerImg.width;
     var coorY = item.location.y + offerImg.height;
@@ -79,5 +85,13 @@ var renderOffers = function (offersArray) {
   return fragment;
 };
 
-map.classList.remove('map--faded');
-mapPinsList.appendChild(renderOffers(generateOffersData(8)));
+var showMap = function () {
+  mapElement.classList.remove('map--faded');
+};
+
+var init = function () {
+  showMap();
+  mapPinsListElement.appendChild(renderOffers(generateOffersData()));
+};
+
+init();
