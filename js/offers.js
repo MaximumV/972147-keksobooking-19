ca3a29ae5a5d7a9bf112cad3628494831bbe9af8
BeHustle <46607810+BeHustle@users.noreply.github.com>
2020-03-06 11:59:46 +0300

    Выносит рендер pop-up карточки в отдельную функцию

--- js/offers.js
+++ js/offers.js
@@ -10,44 +10,48 @@
     .querySelector('.map__card');
   var mapPinsListElement = document.querySelector('.map__pins');
 
-  var renderPins = function (offersArray) {
+  var getOffer = function (offerData) {
+    var offerElement = offerPopupTemplate.cloneNode(true);
+    var offerImagesElement = offerElement.querySelector('.popup__photos');
+    var offerImgTemplate = offerImagesElement.querySelector('.popup__photo').cloneNode(true);
+    offerElement.querySelector('.popup__title')
+      .textContent = offerData.offer.title;
+    offerElement.querySelector('.popup__text--address')
+      .textContent = offerData.offer.address;
+    offerElement.querySelector('.popup__text--price')
+      .textContent = offerData.offer.price + '₽/ночь';
+    offerElement.querySelector('.popup__type')
+      .textContent = window.data.HouseTypes[offerData.offer.type];
+    offerElement.querySelector('.popup__text--capacity')
+      .textContent = offerData.offer.rooms + ' комнаты для ' + offerData.offer.guests + ' гостей';
+    offerElement.querySelector('.popup__text--time')
+      .textContent = 'Заезд после ' + offerData.offer.checkin + ' выезд до ' + offerData.offer.checkout;
+    offerElement.querySelector('.popup__features')
+      .textContent = offerData.offer.features.join(', ');
+    offerElement.querySelector('.popup__description')
+      .textContent = offerData.offer.description;
+    offerImagesElement.removeChild(offerImagesElement.querySelector('.popup__photo'));
+    offerData.offer.photos.forEach(function (item) {
+      var offerImg = offerImgTemplate.cloneNode(true);
+      offerImg.src = item;
+      offerImagesElement.appendChild(offerImg);
+    });
+    offerElement.querySelector('.popup__avatar')
+      .src = offerData.author.avatar;
+    return offerElement;
+  };
+
+  var renderOffers = function (offersArray) {
     var fragment = document.createDocumentFragment();
     for (var i = 0; i < COUNT_OFFERS; i++) {
       var pinElement = mapPinTemplate.cloneNode(true);
-      var offerElement = offerPopupTemplate.cloneNode(true);
       var pinImg = pinElement.querySelector('img');
-      var offerImagesElement = offerElement.querySelector('.popup__photos');
-      var offerImgTemplate = offerImagesElement.querySelector('.popup__photo').cloneNode(true);
       pinElement.style = 'left: ' + offersArray[i].location.x + 'px; top: ' + offersArray[i].location.y + 'px;';
       pinImg.src = offersArray[i].author.avatar;
       pinImg.alt = offersArray[i].offer.title;
-      offerElement.querySelector('.popup__title')
-        .textContent = offersArray[i].offer.title;
-      offerElement.querySelector('.popup__text--address')
-        .textContent = offersArray[i].offer.address;
-      offerElement.querySelector('.popup__text--price')
-        .textContent = offersArray[i].offer.price + '₽/ночь';
-      offerElement.querySelector('.popup__type')
-        .textContent = window.data.HouseTypes[offersArray[i].offer.type];
-      offerElement.querySelector('.popup__text--capacity')
-        .textContent = offersArray[i].offer.rooms + ' комнаты для ' + offersArray[i].offer.guests + ' гостей';
-      offerElement.querySelector('.popup__text--time')
-        .textContent = 'Заезд после ' + offersArray[i].offer.checkin + ' выезд до ' + offersArray[i].offer.checkout;
-      offerElement.querySelector('.popup__features')
-        .textContent = offersArray[i].offer.features.join(', ');
-      offerElement.querySelector('.popup__description')
-        .textContent = offersArray[i].offer.description;
-      offerImagesElement.removeChild(offerImagesElement.querySelector('.popup__photo'));
-      offersArray[i].offer.photos.forEach(function (item) {
-        var offerImg = offerImgTemplate.cloneNode(true);
-        offerImg.src = item;
-        offerImagesElement.appendChild(offerImg);
-      });
-      offerElement.querySelector('.popup__avatar')
-        .src = offersArray[i].author.avatar;
-      fragment.appendChild(offerElement);
       fragment.appendChild(pinElement);
     }
+    fragment.appendChild(getOffer(offersArray[0])); // временно для задания
     mapPinsListElement.appendChild(fragment);
     window.util.hideError();
   };
@@ -57,7 +61,7 @@
   };
 
   var generateOffers = function () {
-    window.backend.load(renderPins, showError);
+    window.backend.load(renderOffers, showError);
   };
 
   window.offers = {
